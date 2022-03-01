3c9d9834792eedd7eb9dd628dcfc2c04b2a3b6fe
readm1 <M.Read1@elsevier.com>
2022-03-01 12:09:57 +0000

    6 - why angular? learn the key features of angular core

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular Core Deep Dive';
+  data = {
+    title: 'Angular Core Deep Dive',
+  };
+
+  onLogoClicked() {
+    alert('Hello World');
+  }
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle;
+  }
 }
