d391db6e0a3054d637fa53fbb393e787431eac04
readm1 <M.Read1@elsevier.com>
2022-03-01 12:50:00 +0000

    9 - component inputs and the @Input decorator

--- angular-course-1-components/src/app/course-card/course-card.component.ts
+++ angular-course-1-components/src/app/course-card/course-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { COURSES } from "../../db-data";
+import { Course } from "../model/course";
 
 @Component({
-  selector: 'course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
 })
 export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
