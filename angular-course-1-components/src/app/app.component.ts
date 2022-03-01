3edb0eddb3e8c6a6cc9365d7bf903bd597e20441
readm1 <M.Read1@elsevier.com>
2022-03-01 16:02:07 +0000

    17 - angular built-in pipes - complete catalogue

--- angular-course-1-components/src/app/app.component.ts
+++ angular-course-1-components/src/app/app.component.ts
@@ -9,6 +9,11 @@ import { Course } from "./model/course";
 })
 export class AppComponent {
   courses = COURSES;
+  title = COURSES[0].description;
+  price = 9.9872367234789;
+  rate = 0.67;
+  startDate = new Date(2000, 0, 1);
+  course = COURSES[0];
 
   onCourseSelected(course: Course) {
     console.log("app component - click event bubbled...", course);
