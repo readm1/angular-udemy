5ae9142216ef81fa670d731428f49a5c6f08fa78
readm1 <M.Read1@elsevier.com>
2022-03-02 14:59:58 +0000

    21 - the viewChildren decorator and query list in detail

--- angular-course-1-components/src/app/app.component.ts
+++ angular-course-1-components/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
 import { COURSES } from "../db-data";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { Course } from "./model/course";
@@ -10,26 +17,19 @@ import { Course } from "./model/course";
 })
 export class AppComponent implements AfterViewInit {
   courses = COURSES;
-  // title = COURSES[0].description;
-  // price = 9.9872367234789;
-  // rate = 0.67;
-  // startDate = new Date(2000, 0, 1);
-  course = COURSES[0];
 
-  @ViewChild("cardRef1", { read: ElementRef })
-  card1: CourseCardComponent;
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
+  cards: QueryList<CourseCardComponent>;
 
-  @ViewChild("container")
-  containerDiv: ElementRef;
+  constructor() {}
 
-  constructor() {
-    console.log("constructor", this.card1);
-  }
   ngAfterViewInit() {
-    console.log("ngAfterViewInit", this.card1);
+    console.log(this.cards);
   }
 
-  onCourseSelected(course: Course) {
-    console.log("onCourseSelected", this.card1);
+  onCourseSelected(course: Course) {}
+
+  onCoursesEdited() {
+    this.courses.push(this.courses[1]);
   }
 }
